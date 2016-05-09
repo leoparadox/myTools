e78657d2af69cca26b1c9c89807252a69c66612e
leoparadox <leparadox@163.com>
2016-05-09 23:28:26 +0800

    test commit

---
+++ calendar.js
@@ -0,0 +1,29 @@
+function getMaxDate(year, month) {
+
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+  }
+
+  if (0 == (year % 4)) {
+    if (0 == (year % 100)) {
+      if (0 == (year % 400)) {
+        return 29;
+      }
+    } else {
+      return 29;
+    }
+  }
+  return 28;
+}
